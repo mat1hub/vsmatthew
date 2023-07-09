07b476badf7034afb4ae66e1368a5450e49993b7
mat1hub <106993617+mat1hub@users.noreply.github.com>
2023-07-09 14:29:29 +0530

    Add files via upload

---
+++ SumOfArrrayValues.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class SumOfArrrayValues
+ {
+ public static void main(String matthewmanasseh[]){
+ int[] array = {10, 20, 30, 40, 50, 10};
+int sum = 0;
+for( int num : array) {
+ sum = sum+num;
+ }
+ System.out.println("Sum of array elements is:"+sum);  }
+ }
