2c4f21b022f0ed25fee95dd08f50d969bdc2fee1
mat1hub <106993617+mat1hub@users.noreply.github.com>
2023-07-13 15:11:03 +0530

    Add files via upload

--- Patient.java
+++ Patient.java
@@ -1,29 +1,53 @@
 package edu.disease.asn1;
-
 import java.util.UUID;
-
 public class Patient {
-	UUID patientId;
-	String firstName;
-	String lastName;
-	Exposure exposures;
-	UUID[] diseaselds;
-	int maxDisease, maxExposures;
-	int index = 0;
-	public Patient(int maxDisease, int maxExposures) {
-		this.maxDisease = maxDisease;
-		this.maxExposures = maxExposures;
-	}
+	
+		UUID patientId;
+		String firstName;
+		String lastName;
+		Exposure[] exposures;
+		UUID[] diseaselds;
+		int maxDisease, maxExposures;
+		int index = 0;
+		int i=0;
+		public Patient(int maxDisease, int maxExposures) {
+			try {
+			if(maxDisease<=0 || maxExposures<=0) {
+				throw new IllegalArgumentException("invalid data");
+			}
+			else {
+				diseaselds = new UUID[maxDisease];
+				this.maxExposures = maxExposures;
+			}
+			}
+			catch(IllegalArgumentException e) {
+				
+			}
+			
+		}
 
 
-	public void addDiseaseld(UUID diseaseld) {
-		if(index < maxDisease)
-			diseaselds[index++]=diseaseld; 
-		else
-			throw new ArrayIndexOutOfBoundsException("memory overloaded!");
-	}
-	public void addExposure(Exposure exposures ) {
+		public void addDiseaseld(UUID diseaseld) {
+			try {
+			if(index < maxDisease)
+				diseaselds[index++]=diseaseld; 
+			
+			else {
+				throw new ArrayIndexOutOfBoundsException("memory overloaded!");
+			}
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+			}
+		}
 		
-	}
-
+		public void addExposure(Exposure exposures ) {
+				
+				
+			this.exposures[i++]=exposures;
+			if() {
+				
+			}
+			}
 }
+
+	
