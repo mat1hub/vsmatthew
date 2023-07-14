d5ad2e591ae723ccc0628607b78a116c0f43779b
mat1hub <106993617+mat1hub@users.noreply.github.com>
2023-07-14 18:29:41 +0530

    Add files via upload

---
+++ lab 8/Wrapp.java
@@ -0,0 +1,15 @@
+public class Wrapp {
+	static int a =10;
+	static float f=10.2f;
+	static boolean flag;
+public static void main(String[] args) {
+	Integer i2 =Integer.valueOf(a);// wrapper class
+	Float i3 =Float.valueOf(f);
+	Boolean b = Boolean.valueOf(flag);
+	Integer i4 = 24;// Integer.value(24) - autoboxing
+	int  k =i2; //auto unboxing 
+
+    	
+	
+}
+}
