401466d53537dd9eaffb9da25fa71a573a40367a
mat1hub <106993617+mat1hub@users.noreply.github.com>
2023-08-20 20:29:52 +0530

    DiseaseControlManagerImp.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn2/DiseaseControlManagerImp.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn2/DiseaseControlManagerImp.java
@@ -7,16 +7,38 @@ import edu.disease.asn1.Exposure;
 public class DiseaseControlManagerImp implements DiseaseControlManager {
 	private Disease[] diseases;
 	private Patient[] patients;
-
+	int md,me,indexdisease=0,indexpatient=0;
 	public DiseaseControlManagerImp(int maxDisease, int maxpatients) {
-		if(maxDisease<=0 || maxpatients<=0) {
-			
+		if(maxDisease<=0 || maxpatients<=0) 
+			{
+			   throw new IllegalArgumentException("Invalid Data");
+			}
+		else {
+			  diseases=new Disease[maxDiseases];
+			  patients=new Patient[maxPatient];
+	              }
+
+	@Override
+	public Disease addDisease(String name, boolean infectious) {
+                if (md <= indexdisease) {
+			throw new IllegalStateException("No more Disease can be added");
+		} else {
+			if (infectious) {
+				return diseases[indexd++] = new InfectiousDisease();
+			} else {
+				return diseases[indexd++] = new NonInfectiousDisease();
+			}
 		}
 	}
 
 	@Override
-	public Disease addDisease(String name, boolean infectious) {
-
+	public Disease getDisease(UUID diseaseId) {
+		for(Disease d:diseases) {
+			if(d.diseaseId==diseaseId) {
+				return d;
+			}
+			
+		}
 		return null;
 	}
 
@@ -32,29 +54,83 @@ public class DiseaseControlManagerImp implements DiseaseControlManager {
 
 	@Override
 	public Patient addPatient(String firstName, String LastName, int maxDiseases, int maxExposures) {
+		if (me <= indexpatient) {
+			throw new IllegalStateException("No more Patients can be added");
+		} else {
+			patients[indexpatient]=new Patient(maxDiseases, maxExposures);
+			patients[indexpatient].setFirstName(firstName);
+			patients[indexpatient].setLastName(lastName);
+			return patients[indexpatient++];
+		}
 		
-		return null;
+		
 	}
 
 	@Override
 	public Patient getPatient(UUID patientId) {
-		 for(Patient patient:patients) {
-		    	if(patient.getPatientId()==patientId) {
-		    		return patient;
-		    	}
-		    }
+		// TODO Auto-generated method stub
+		for(Patient p:patients) {
+			if(p.patientId==patientId) {
+				return p;
+			}
+		}		
+		
 		return null;
 	}
 
+	
+
 	@Override
 	public void addDiseaseToPatient(UUID patientId, UUID diseaseId) {
-	
+	Disease ds=null;
+		Patient ps=null;
+		int dg=0,pg=0;
+		for (Disease d : diseases) {
+			if (d.diseaseId == diseaseId) {
+				ds=d;
+				dg=1;
+			}
+		}
+		if(dg==0)
+		{
+			throw new IllegalArgumentException("Disease not Found");
+		}
+		for (Patient p : patients) {
+			if (p.patientId == patientId) {
+				ps=p;
+				pg=1;
+			}
+		}
+		if(pg==0)
+		{
+			throw new IllegalArgumentException(" No Patient was found...>>>!");
+		}
+		if(dg==1&&pg==1)
+		{
+			ps.addDiseaseId(ds.diseaseId);
+		}
+		
 
 	}
 
 	@Override
 	public void addExposureToPatient(UUID patientId, Exposure exposures) {
-	
+	int pg=0;
+		Patient ps=null;
+		for (Patient p : patients) {
+			if (p.patientId == patientId) {
+				ps=p;
+				pg=1;
+			}
+		}
+		if(pg==0)
+		{
+			throw new IllegalArgumentException("No patient was found...!");
+		}
+		else {
+			ps.addExposure(exposure);
+		}
+	}
 
 	}
 
