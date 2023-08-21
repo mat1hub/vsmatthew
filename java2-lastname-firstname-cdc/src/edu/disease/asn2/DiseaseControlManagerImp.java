ffa4a0c7706349fd32e11644513d78842bbd4fbc
mat1hub <106993617+mat1hub@users.noreply.github.com>
2023-08-21 09:35:12 +0530

    DiseaseControlManagerImp.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn2/DiseaseControlManagerImp.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn2/DiseaseControlManagerImp.java
@@ -54,14 +54,7 @@ public class DiseaseControlManagerImp implements DiseaseControlManager {
 
 	@Override
 	public Patient addPatient(String firstName, String LastName, int maxDiseases, int maxExposures) {
-		if (me <= indexpatient) {
-			throw new IllegalStateException("No more Patients can be added");
-		} else {
-			patients[indexpatient]=new Patient(maxDiseases, maxExposures);
-			patients[indexpatient].setFirstName(firstName);
-			patients[indexpatient].setLastName(lastName);
-			return patients[indexpatient++];
-		}
+		
 		
 		
 	}
