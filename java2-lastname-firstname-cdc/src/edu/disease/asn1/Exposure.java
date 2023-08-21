0cd535d8b86b06e493bf64ba0cee289ed8514c73
mat1hub <106993617+mat1hub@users.noreply.github.com>
2023-08-21 18:21:24 +0530

    Exposure.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn1/Exposure.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn1/Exposure.java
@@ -3,74 +3,114 @@ package edu.disease.asn1;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+/**
+ * The class Exposure represents an exposure event that occurred to a patient.
+ * It includes information about the patient, the exposure date and time, and the type of exposure.
+ */
 public class Exposure {
-	UUID patientId;
-	LocalDateTime dateTime;
-	String exposureType;
-	
-	public Exposure(LocalDateTime dateTime, String exposureType) {
-		this.dateTime = dateTime;
-		this.exposureType = exposureType;
-	}
 
-	public LocalDateTime getDateTime() {
-		return dateTime;
-	}
+    /** The unique identifier of the patient. */
+    UUID patientId;
 
-	public void setDateTime(LocalDateTime dateTime) {
-		this.dateTime = dateTime;
-	}
+    /** The date and time of the exposure event. */
+    LocalDateTime dateTime;
 
-	public String getExposureType() {
-		return exposureType;
-	}
+    /** The type of exposure, either "D" (direct) or "I" (indirect). */
+    String exposureType;
 
-	public void setExposureType(String exposureType) {
-		if((exposureType.equalsIgnoreCase("D")) || (exposureType.equalsIgnoreCase("I")))
-			this.exposureType = exposureType;
-		else
-			throw new IllegalArgumentException("Kindly Enter 'D' or 'I' !");
-	}
-	
-	public UUID getPatientId() {
-		return patientId;
-	}
-	
+    /**
+     * Constructor to create an instance of Exposure with a specified patient ID.
+     *
+     * @param patientId The unique identifier of the patient associated with the exposure.
+     */
+    public Exposure(UUID patientId) {
+        this.patientId = patientId;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dateTime == null) ? 0 : dateTime.hashCode());
-		result = prime * result + ((patientId == null) ? 0 : patientId.hashCode());
-		return result;
-	}
+    /**
+     * Gets the date and time of the exposure event.
+     *
+     * @return The date and time of the exposure event.
+     */
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Exposure other = (Exposure) obj;
-		if (dateTime == null) {
-			if (other.dateTime != null)
-				return false;
-		} else if (!dateTime.equals(other.dateTime))
-			return false;
-		if (patientId == null) {
-			if (other.patientId != null)
-				return false;
-		} else if (!patientId.equals(other.patientId))
-			return false;
-		return true;
-	}
+    /**
+     * Sets the date and time of the exposure event.
+     *
+     * @param dateTime The date and time of the exposure event to set.
+     */
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 
-	@Override
-	public String toString() {
-		return "Exposure [patientId=" + patientId + ", dateTime=" + dateTime + ", exposureType=" + exposureType + "]";
-	}
-	
+    /**
+     * Gets the type of exposure.
+     *
+     * @return The type of exposure ("D" for direct or "I" for indirect).
+     */
+    public String getExposureType() {
+        return exposureType;
+    }
+
+    /**
+     * Sets the type of exposure.
+     *
+     * @param exposureType The type of exposure to set ("D" for direct or "I" for indirect).
+     * @throws IllegalArgumentException if the provided exposure type is not "D" or "I".
+     */
+    public void setExposureType(String exposureType) {
+        if ((exposureType.equalsIgnoreCase("D")) || (exposureType.equalsIgnoreCase("I")))
+            this.exposureType = exposureType;
+        else
+            throw new IllegalArgumentException("Kindly Enter 'D' or 'I' !");
+    }
+
+    /**
+     * Gets the unique identifier of the patient associated with the exposure.
+     *
+     * @return The patient's unique identifier.
+     */
+    public UUID getPatientId() {
+        return patientId;
+    }
+
+    // Overrides for hashCode, equals, and toString methods
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((dateTime == null) ? 0 : dateTime.hashCode());
+        result = prime * result + ((patientId == null) ? 0 : patientId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Exposure other = (Exposure) obj;
+        if (dateTime == null) {
+            if (other.dateTime != null)
+                return false;
+        } else if (!dateTime.equals(other.dateTime))
+            return false;
+        if (patientId == null) {
+            if (other.patientId != null)
+                return false;
+        } else if (!patientId.equals(other.patientId))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Exposure [patientId=" + patientId + ", dateTime=" + dateTime + ", exposureType=" + exposureType + "]";
+    }
 }
